51a2be131d0f9b3d71f0b628e3f141952887331a
antaresrvish <yildirimyusuf758@gmail.com>
2025-03-09 12:44:40 +0300

    feat: enhance register input functionality and introduce bus interface

--- src/app.ts
+++ src/app.ts
@@ -11,26 +11,51 @@ interface register {
     clr: boolean;
 };
 
-const registerIn = (reg: register, input: number): register => {
+interface bus {
+    data: number;
+}
+
+let bus: bus = {
+    data: 0b00000000
+}
+
+const registerIn = (reg: register, data: number): register => {
     if(reg.clr) {
         reg.data = 0b00000000;
+        console.log(1)
         return reg;
     }
 
     if(reg.clk && reg.in) {
-        reg.data = input;
+        reg.data = data;
+        console.log(2)
+    }
+    
+    if(reg.clk && reg.out) {
+        bus.data = reg.data;
+        console.log(3)
     }
 
     return reg;
 };
 
 let aRegister: register = {
-    data: 0b00000000,
+    data: 0b00000001,
     out: false,
     in: true,
     clk: true,
     clr: false,
 };
-console.log(aRegister)
+
+let bRegister: register = {
+    data: 0b00000010,
+    out: true,
+    in: false,
+    clk: true,
+    clr: false
+}
+console.log('aRegister',aRegister)
+console.log('bRegister',bRegister)
 aRegister = registerIn(aRegister, 0b00001111);
-console.log(aRegister)
+bRegister = registerIn(bRegister, 0b00001111);
+console.log('bus',bus);
