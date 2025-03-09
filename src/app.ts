ee7ad7bfecd7237f855fed2184d94cb2f9c71a75
antaresrvish <yildirimyusuf758@gmail.com>
2025-03-09 01:19:37 +0000

    feat: add CPU and register interfaces with register input functionality

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,36 @@
-console.log('a')
+interface cpu {
+    a: register;
+    b: register;
+};
+
+interface register {
+    data: number;
+    out: boolean;
+    in: boolean;
+    clk: boolean;
+    clr: boolean;
+};
+
+const registerIn = (reg: register, input: number): register => {
+    if(reg.clr) {
+        reg.data = 0b00000000;
+        return reg;
+    }
+
+    if(reg.clk && reg.in) {
+        reg.data = input;
+    }
+
+    return reg;
+};
+
+let aRegister: register = {
+    data: 0b00000000,
+    out: false,
+    in: true,
+    clk: true,
+    clr: false,
+};
+console.log(aRegister)
+aRegister = registerIn(aRegister, 0b00001111);
+console.log(aRegister)
